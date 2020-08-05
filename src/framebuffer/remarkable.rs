a1b9d4b8f9b1c8150482d4a1c350dff78c598a5e
Linus <linus@cosmos-ink.net>
2020-08-05 18:53:33 +0200

    WIP: Rotation working with unpublished changes to libremarkable

--- src/framebuffer/remarkable.rs
+++ src/framebuffer/remarkable.rs
@@ -17,6 +17,10 @@ use libremarkable::framebuffer::common;
 use std::convert::TryInto;
 use anyhow::anyhow;
 use crate::view::RefreshQuality;
+use mmap;
+use mmap::MemoryMap;
+use std::os::unix::io::AsRawFd;
+
 
 type SetPixelRgb = fn(&mut RemarkableFramebuffer, u32, u32, [u8; 3]);
 type GetPixelRgb = fn(&RemarkableFramebuffer, u32, u32) -> [u8; 3];
@@ -31,14 +35,31 @@ pub struct RemarkableFramebuffer {
 
 impl RemarkableFramebuffer {
     pub fn new(fb_device_path: &'static str) -> Result<RemarkableFramebuffer, Error> {
-        let fb = libremarkable::framebuffer::core::Framebuffer::new(fb_device_path);
+        let mut fb = libremarkable::framebuffer::core::Framebuffer::new(fb_device_path, 1);
+        println!("X: {}, Y: {} | XV: {}, YV: {}",
+        fb.var_screen_info.xres, fb.var_screen_info.yres,
+        fb.var_screen_info.xres_virtual, fb.var_screen_info.yres_virtual);
+        //fb.update_var_screeninfo();
+        // X: 1404, Y: 1872 | XV: 1408, YV: 3840 <--- WTF?!?!?
+        // If not mistaken, i managed landscape mode
+        // at the very beginning of the affecting commit
+        // maybe just pretent to be rotated?
+        // rotation() = xxx (- 1) ??
+
+        // Also fact: Using two fingers = UI reaction messed up!
+        // Maybe check the finger recognition for trailing ones.
 
         Ok(RemarkableFramebuffer { fb, monochrome: false, inverted: false, refresh_quality: RefreshQuality::Normal })
     }
 
     fn set_pixel_rgb(&mut self, x: u32, y: u32, rgb: [u8; 3]) {
         let [red, green, blue] = rgb;
-        self.fb.write_pixel(cgmath::Point2 { x: x as i32, y: y as i32 }, common::color::RGB(red, green, blue));
+        /*if self.fb.var_screen_info.rotate % 2 == 0 {
+            // Swap x and y
+            self.fb.write_pixel(cgmath::Point2 { x: y as i32, y: x as i32 }, common::color::RGB(red, green, blue));
+        }else {*/
+            self.fb.write_pixel(cgmath::Point2 { x: x as i32, y: y as i32 }, common::color::RGB(red, green, blue));
+        //}
     }
 
     fn get_pixel_rgb(&self, x: u32, y: u32) -> [u8; 3] {
@@ -57,7 +78,12 @@ impl Framebuffer for RemarkableFramebuffer {
 
     fn set_pixel(&mut self, x: u32, y: u32, color: u8) {
         // color seems to be inverted! Either in color::GRAY or from plato.
-        self.fb.write_pixel(cgmath::Point2 { x: x as i32, y: y as i32 }, common::color::GRAY(255 - color));
+        /*if self.fb.var_screen_info.rotate % 2 == 0 {
+            // Swap x and y
+            self.fb.write_pixel(cgmath::Point2 { x: y as i32, y: x as i32 }, common::color::GRAY(255 - color));
+        }else {*/
+            self.fb.write_pixel(cgmath::Point2 { x: x as i32, y: y as i32 }, common::color::GRAY(255 - color));
+        //}
     }
 
     fn set_blended_pixel(&mut self, x: u32, y: u32, color: u8, alpha: f32) {
@@ -108,6 +134,18 @@ impl Framebuffer for RemarkableFramebuffer {
             width: rect.max.x as u32 - rect.min.x as u32,
             height: rect.max.y as u32 - rect.min.y as u32,
         };
+        println!("{:?} -> {:?}", mode, new_rect);
+        
+
+        if false {
+            return Ok(self.fb.full_refresh(
+                    common::waveform_mode::WAVEFORM_MODE_GC16, // Flashes black white in full mode
+                    common::display_temp::TEMP_USE_AMBIENT, // Not such low latency (see comments on this)
+                    common::dither_mode::EPDC_FLAG_USE_REMARKABLE_DITHER, // Good or bad here???
+                    common::DRAWING_QUANT_BIT,
+                    false, // Don't wait for completion (token should allow the device to do anyway if actually wanted)
+                ));
+        }
 
         // Note: I took some of the comments from libremarkable
         // regarding those settings and rephrased them here for
@@ -263,17 +301,38 @@ impl Framebuffer for RemarkableFramebuffer {
     fn set_rotation(&mut self, n: i8) -> Result<(u32, u32), Error> {
         // This will probably not work.
         // Not sure if the result is even correct.
-        self.fb.var_screen_info.rotate = n as u32;
-        self.fb.update_var_screeninfo();
+        println!("New FB-Rot: {}", n);
+
+        self.fb = libremarkable::framebuffer::core::Framebuffer::new("/dev/fb0", n as u32);
+        
+        //self.fb.var_screen_info.rotate = n as u32;
+        //self.fb.update_var_screeninfo();
+        println!("X: {}, Y: {} | XV: {}, YV: {}",
+        self.fb.var_screen_info.xres, self.fb.var_screen_info.yres,
+        self.fb.var_screen_info.xres_virtual, self.fb.var_screen_info.yres_virtual);
+
+        /*let frame_length = (self.fb.fix_screen_info.line_length * self.fb.var_screen_info.yres) as usize;
+        let mem_map = MemoryMap::new(
+            frame_length,
+            &[
+                mmap::MapOption::MapReadable,
+                mmap::MapOption::MapWritable,
+                mmap::MapOption::MapFd(self.fb.device.as_raw_fd()),
+                mmap::MapOption::MapOffset(0),
+                mmap::MapOption::MapNonStandardFlags(libc::MAP_SHARED),
+            ],
+        )
+        .unwrap();
+        self.fb.frame = mem_map;*/
 
         // Assume that rotations 0 and 2 are portrait
         // and 1 and 3 are landscape (switched axes)
         // Is the return type even supposed to be a size??
-        if n % 2 == 0 {
+        //if n % 2 == 0 {
             Ok((self.width(), self.height()))
-        }else {
-            Ok((self.height(), self.width()))
-        }
+        //}else {
+            //Ok((self.height(), self.width()))
+        //}
     }
 
     fn set_inverted(&mut self, enable: bool) {
