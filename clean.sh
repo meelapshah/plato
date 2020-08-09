f2547750c7dd1d711d292bf8e04f4c55aa15f7a3
Linus <linus@cosmos-ink.net>
2020-08-09 23:41:22 +0200

    Fix clean.sh

--- clean.sh
+++ clean.sh
@@ -6,7 +6,7 @@ rm -rf dist/ libs/
 
 rm -f "plato-*.zip"
 
-if [ ! -z "mupdf-*.tar.xz" ]; then
+if ls | grep "mupdf-*.tar.xz" >/dev/null 2>&1; then
   # download_headers.sh was likely executed to get headers
   rm -rf thirdparty/mupdf/include
   rm "mupdf-*.tar.xz"
